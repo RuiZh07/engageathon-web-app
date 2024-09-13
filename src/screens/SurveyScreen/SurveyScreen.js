bd4e3a0cabd4ce401e08904ec123df778f1abe2c
Rui Zhao <ruizhao@Ruis-MacBook-Pro.local>
2024-09-12 21:53:52 -0500

    survey

--- src/screens/SurveyScreen/SurveyScreen.js
+++ src/screens/SurveyScreen/SurveyScreen.js
@@ -1,44 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
-import './SurveyScreen.scss';
-import backgroundImage from '../../assets/backgroundImage.png';
-import IconTitle from '../../components/IconTitle/IconTitle';
 import MainButton from '../../components/MainButton/MainButton';
-import prize from '../../assets/prizes/prize.png';
-import starBadge from '../../assets/prizes/star-badge.png';
+import './SurveyScreen.scss';
 
 export default function SurveryScreen() {
     const navigate = useNavigate();
 
     const handlePress = () => {
-        navigate('/image-slider'); 
+        navigate('congratulations'); 
     };
 
   return (
     <div className="container">
-        <img
-            src={backgroundImage}
-            alt="Background"
-            className="background-image"
-        />
-        
-        <div className="confirmation-content-container">
-        
+        <div className="feedbackContentContainer">
             <div className="greatJobContainer">
-                <h1 className="welcome-text">Rate your<br />experience</h1>
+                <h1 className="rateText">Rate your<br />experience</h1>
                 <div style={{ marginTop: '20px' }}>
-                    <p className="overviewText">Share your experience to help us improve and earn extra rewards</p>
+                    <p className="shareText">Share your experience to help us improve and earn extra rewards</p>
                  </div>
             </div>
             <div className="feedbackContainer">
-            <input type="text" placeholder="Share your experience with us..." className="feedbackInput" />
+                <input type="text" placeholder="Share your experience with us..." className="feedbackInput" />
             </div>
         </div>
-        <div className="submitButtonContainer">
+        <div className="button-container">
             <MainButton title="Submit" onClick={handlePress} />
         </div>
-        <button class="skipButton">Skip</button>
 
+        {/*
+        <button class="skipButton" onClick={handlePress}>Skip</button>
+        */}
+        
     </div>
   );
 }
