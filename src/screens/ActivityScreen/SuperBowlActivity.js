86c8bc11b1f844f9d0bf68a5c349e1a3f01cc9ea
Rui Zhao <rzhao7815@gmail.com>
2025-03-11 09:42:06 -0500

    New file for SuperBowl event

---
+++ src/screens/ActivityScreen/SuperBowlActivity.js
@@ -0,0 +1,346 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom'; 
+import { FaArrowRight } from "react-icons/fa6";
+import { TbRosetteDiscountCheckFilled } from "react-icons/tb";
+import prize from '../../assets/prizes/prize.png';
+import starBadge from '../../assets/prizes/star-badge.png';
+import genres from '../../assets/icons/genres.png';
+import phoneCamera from '../../assets/icons/smartphone_camera.png';
+import QRScanner from "../../components/QRScanner/QRScanner";
+import CameraCapture from '../../components/CameraCapture/CameraCapture';
+import MainButton from '../../components/MainButton/MainButton';
+import './SuperBowlActivity.scss';
+
+import cigars from "../../assets/superbowl/cigars.png";
+import dance from "../../assets/superbowl/dance.png";
+import collegeDay from "../../assets/superbowl/collegeDay.png";
+import gala from "../../assets/superbowl/gala.png";
+import gaming from "../../assets/superbowl/gaming.png";
+import sipSmoke from "../../assets/superbowl/sip_smoke.png";
+import masquerade from "../../assets/superbowl/masquerade.png";
+import bigGame from "../../assets/superbowl/big_game.png";
+
+export default function SuperBowlActivity() {
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [activities, setActivities] = useState([]);
+    const [completedActivityID, setCompletedActivityID] = useState(null);
+    const [totalPoints, setTotalPoints] = useState(0);
+    const [totalBadges, setTotalBadges] = useState(0);
+    const [unconfirmedCount, setUnconfirmedCount] = useState(0);
+    const [totalPossiblePoints, setTotalPossiblePoints] = useState(0);
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const [isCameraOpen, setIsCameraOpen] = useState(false);
+    const [isCameraCaptureOpen, setIsCameraCaptureOpen] = useState(false);
+    const [activeActivity, setActiveActivity] = useState({});
+    const firstColumn = [];
+    const secondColumn = [];
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    // Split activites into two columns for UI rendering
+    for (let i = 0; i < activities.length; i++) {
+        if (i % 2 === 0) {
+            firstColumn.push(activities[i]);  
+        } else {
+            secondColumn.push(activities[i]);
+        }
+    }
+
+    // Fetch the user data
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const userData = localStorage.getItem('userData'); 
+            if (userData) {
+                const { first_name, email } = JSON.parse(userData);
+                setEmail(email);
+                console.log("userData", email);
+                setUserName(first_name);
+                fetchActivities(email);
+            }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+            }
+        };
+
+        fetchUserData();
+    }, []);
+
+    // Fetch activities associated witht the user's email
+    const fetchActivities = async (email) => {
+        try {
+            if (!email) {
+                console.error('No email found');
+                return;
+            }
+
+            const response = await fetch(
+                `https://app.engageathon.com/api/events/activity/9/${email}/`, 
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            const data = await response.json();
+            console.log(data);
+            setActivities(data.activities || []);
+            setTotalPoints(data.accumulated_points || 0);
+            setTotalBadges(data.confirmed_count || 0);
+            setUnconfirmedCount(data.unconfirmed_count || 0);
+            setTotalPossiblePoints(data.total_possible_points || 0);
+        } catch (error) {
+            console.error('Error fetching activities', error);
+        }
+    };
+
+    useEffect(() => {
+        if (!isCameraOpen) {
+            setActiveActivity({});
+        }
+    }, [isCameraOpen]);
+    
+    // Update activities list when a specific activity is confirmed
+    useEffect(() => {
+        if (location.state && location.state.completedActivityId) {
+            const { completedActivityId } = location.state;
+            setCompletedActivityID(completedActivityId);
+
+            // Find the activity that matched the completedActivityId
+            const completedActivity = activities.find(
+                (activity) => activity.id === completedActivityId
+            );
+
+            if (completedActivity && !completedActivity.confirmed) {
+                const updatedActivities = activities.map((activity) =>
+                    activity.id === completedActivity.id
+                    ? { ...activity, confirmed: true }
+                    : activity
+                );
+                setActivities(updatedActivities);
+            }
+        }
+    }, [location.state, activities]);
+
+    const handleContinue = () => {
+        navigate("/collect-rewards");
+        //setModalVisible(false);
+    };
+
+    const handleFinishEngaging = () => {
+        navigate("/journey-choice");
+    };
+
+    // Handle when an activity is clicked
+    const handleActivityPress = (activity) => {
+        if (!activity.confirmed) {
+            setIsCameraOpen(true);
+            setIsCameraCaptureOpen(false);
+            setActiveActivity(activity);
+        } else {
+            alert("Already Completed", "This activity has already been completed.");
+        }
+    };
+
+    // Function to mark activity as confirmed by submitting a token
+    const setActivityConfirmed = async (token) => {
+        const userData = localStorage.getItem("userData");
+        if (userData) {
+            const { email } = JSON.parse(userData);
+            try {
+                const response = await fetch(
+                    `https://app.engageathon.com/api/events/activity/scan/9/${activeActivity?.activity}/`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            token: token,
+                            email: email,
+                        }),
+                    }
+                );
+
+                const result = await response.json();
+
+                if (response.ok) {
+                    console.log("Response Body:", result);
+                    fetchActivities(email);
+                    navigate("/congrats-activity", {
+                        state: {
+                            activityId: activeActivity.activity,
+                            badgeName: activeActivity.badge,
+                            activityPoints: activeActivity.activity_points,
+                            email: email,
+                        },
+                    });
+                } else {
+                    alert(
+                    result.error ||
+                    "An error occurred. Please check your network connection."
+                );
+                }
+            } catch (error) {
+                alert("An error occurred. Please check your network connection.");
+            }
+        } else {
+            alert("An error occurred. Please check your network connection.");
+        }
+    };
+    
+    return (
+      <div className="container">
+        <div className="scrollContent">
+            <div className="titleContainer">
+                <p className="userName">Hi, {userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase()}</p>
+                <button className="finishEngaging" onClick={handleFinishEngaging}>Finish Engaging</button>
+            </div>
+            <p className="scanQRText">Scan the QR code at each station to earn points and badges</p>
+            <p className="rewardsEarnedText">Rewards Earned</p>
+            <div className="rewardArea">
+                <div className="rewardsContainer">
+                    <img src={starBadge} alt="badge" className="badgeIcon" />
+                    <div className="pointsContainer">
+                        <p className="getPointsActivity">{totalPoints}</p>
+                        <p className="totalPointActivity">of {totalPossiblePoints} Points</p>
+                    </div>
+                </div>
+                <div className="rewardsContainer">
+                    <img src={prize} alt="prize" className="prizeIcon" />
+                    <div className="pointsContainer">
+                        <p className="getPointsActivity">{totalBadges}</p>
+                        <p className="totalPointActivity">of {unconfirmedCount} Badges</p>
+                    </div>
+                </div>
+            </div>
+            <p className="activitiesText">Activities</p>
+
+            <div className="activityArea">  
+                <div className="activityColumn">
+                    {firstColumn.map((activity) => (
+                        <button key={activity.id} className="activityContainer" onClick={() => handleActivityPress(activity)}>
+                            <div className="iconsContainer">
+                                {activity.activity === 95 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={sipSmoke} className="superbowlIcon" alt="sip smoke" />
+                                    </div>
+                                )}
+                                {activity.activity === 96 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={masquerade} className="superbowlIcon" alt="masquerade" />
+                                    </div>
+                                )}
+                                {activity.activity === 97 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={bigGame} className="superbowlIcon" alt="big game" />
+                                    </div>
+                                )}
+                                
+                                {activity.confirmed ? (
+                                    <TbRosetteDiscountCheckFilled 
+                                    size={28} 
+                                    color="#32a852" 
+                                    className="arrowIcon" 
+                                    />
+                                ) : (
+                                    <FaArrowRight 
+                                    size={20} 
+                                    color="#FFFFFF" 
+                                    className="arrowIcon" 
+                                    />
+                                )}
+                            </div>
+
+                            <p className="activityNameText">{activity.activity_name}</p>
+                            <p className="pointText">
+                                {activity.confirmed 
+                                ? "Points Claimed" 
+                                : `${activity.activity_points} Points`}
+                            </p>
+                        </button>
+                    ))}
+                </div>
+              
+                <div className="activityColumn">
+                    {secondColumn.map((activity) => (
+                        <button key={activity.id} className="activityContainer" onClick={() => handleActivityPress(activity)}>
+                            <div className="iconsContainer">
+                            {activity.activity === 95 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={sipSmoke} className="superbowlIcon" alt="sip smoke" />
+                                    </div>
+                                )}
+                                {activity.activity === 96 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={masquerade} className="superbowlIcon" alt="masquerade" />
+                                    </div>
+                                )}
+                                {activity.activity === 97 && (
+                                    <div className="superbowlIconBackground">
+                                        <img src={bigGame} className="superbowlIcon" alt="big game" />
+                                    </div>
+                                )}
+
+                                {activity.confirmed ? (
+                                    <TbRosetteDiscountCheckFilled 
+                                    size={28} 
+                                    color="#32a852" 
+                                    className="arrowIcon" 
+                                    />
+                                ) : (
+                                    <FaArrowRight 
+                                    size={20} 
+                                    color="#FFFFFF" 
+                                    className="arrowIcon" 
+                                    />
+                                )}
+                            </div>
+
+                            <p className="activityNameText">{activity.activity_name}</p>
+                            <p className="pointText">
+                                {activity.confirmed 
+                                ? "Points Claimed" 
+                                : `${activity.activity_points} Points`}
+                            </p>
+                        </button>
+                    ))}
+                    </div>
+                </div>
+            </div>
+
+            {isModalVisible && (
+                <div className="modalContainer">
+                    <div className="modalContent">
+                        <p className="modalTitle">Congratulations!</p>
+                        <p className="modalMessage">
+                            You have completed all the activities!
+                        </p>
+                        <div className="modalButtonContainer">
+                            <MainButton title="Continue" onClick={handleContinue} />
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {isCameraOpen && (
+                <QRScanner
+                    setActivityConfirmed={setActivityConfirmed}
+                    setIsCameraOpen={setIsCameraOpen}
+                />
+            )}
+
+            {isCameraCaptureOpen && (
+                <CameraCapture
+                    setIsCameraCaptureOpen={setIsCameraCaptureOpen}
+                    setIsCameraOpen={setIsCameraOpen}
+                />
+            )}
+        </div>
+    );
+}
